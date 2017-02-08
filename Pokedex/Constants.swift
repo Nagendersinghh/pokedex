a0359da5c1e36b0b3ce95e1626a55a0cdecaa3e2
Nagender Singh <nagendersinghh@gmail.com>
2017-02-08 08:54:12 +0530

    Initial Commit

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by nagender singh shekhawat on 14/02/17.
+//  Copyright © 2017 nagender singh shekhawat. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias downloadComplete = () -> ()
